89f0babba882413b6c0574ac9758e1f9c6eb9b04
Daria Klimova <d.klimova@yandex.ru>
2023-01-17 22:27:29 +0200

    Update tests.py

--- tests.py
+++ tests.py
@@ -1,101 +1,68 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+@pytest.fixture(autouse=True)
+def testing():
+   pytest.driver = webdriver.Chrome('./chromedriver.exe')
+   # Переходим на страницу авторизации
+   pytest.driver.get('http://petfriends.skillfactory.ru/login')
+   yield
+   pytest.driver.quit()
 
-@pytest.fixture
-def driver():
-    _driver = webdriver.Chrome('./chromedriver.exe')
+def test_show_my_pets():
+   # Вводим email
+   pytest.driver.find_element_by_id('email').send_keys('d.klimova@yandex.ru')
+   # Вводим пароль
+   pytest.driver.find_element_by_id('pass').send_keys('13031985A')
+   # Нажимаем на кнопку входа в аккаунт
+   pytest.driver.find_element_by_css_selector('button[type="submit"]').click()
+   # Нажимаем на кнопку бургера
+   pytest.driver.find_element_by_xpath('//span[@class="navbar-toggler-icon"]').click()
+   # Нажимаем на кнопку "Мои питомцы"
+   pytest.driver.find_element_by_xpath('//a[@class="nav-link" and @href="/my_pets"]').click()
+   # Сохраняем в переменную элементы статистики
+   pets_statistic = pytest.driver.find_elements_by_xpath('//div[@class=".col-sm-4 left"]')
+   # Получаем количество питомцев из данных статистики
+   number = pets_statistic[0].text.split('\n')
+   number = number[1].split(' ')
+   number = int(number[1])
+   # Находим количество питомцев на странице
+   pets = WebDriverWait(pytest.driver, 10).until(EC.presence_of_all_elements_located((By.XPATH, "//tbody/tr")))
+   #pets = pytest.driver.find_elements_by_xpath('//tbody/tr')
+   # Сохраняем в переменную количество питомцев с фото и без фото
+   pytest.driver.implicitly_wait(5)
+   with_photo = pytest.driver.find_elements_by_xpath('//img[contains(@src, "jpeg")]')
+   without_photo = pytest.driver.find_elements_by_xpath('//img[@src=""]')
+   # Сохраняем имена питомцев в переменную
+   names = pytest.driver.find_elements_by_xpath('//tbody/tr/td[1]')
+   # Сохраняем породу питомцев в переменную
+   breed = pytest.driver.find_elements_by_xpath('//tbody/tr/td[2]')
+   # Сохраняем возраст питомцев в переменную
+   age = pytest.driver.find_elements_by_xpath('//tbody/tr/td[3]')
 
-    yield _driver
-
-    _driver.quit()
-
-
-@pytest.fixture
-def authorized_driver(driver):
-    driver.get('http://petfriends.skillfactory.ru/login')
-    driver.find_element_by_id('email').send_keys('d.klimova@yandex.ru')
-    driver.find_element_by_id('pass').send_keys('13031985A')
-    driver.find_element_by_css_selector('button[type="submit"]').click()
-
-    return driver
-
-
-def test_show_my_pets(authorized_driver):
-    driver = authorized_driver
-    driver.get('http://petfriends.skillfactory.ru/my_pets')
-    images = driver.find_elements_by_css_selector('.card-deck .card-img-top')
-    names = driver.find_elements_by_css_selector('.card-deck .card-img-top')
-    descriptions = driver.find_elements_by_css_selector('.card-deck .card-img-top')
-
-    for i in range(len(names)):
-        assert images[i].get_attribute('src') != '', "Image not found"
-        assert names[i].text != ''
-        assert descriptions[i].text != ''
-        assert ', ' in descriptions[i]
-        kind, age = descriptions[i].text.split(", ")
-        assert len(kind) > 0
-        assert len(age) > 0
-
-
-@pytest.fixture
-def my_pets(authorized_driver):
-    authorized_driver.implicitly_wait(2)
-    authorized_driver.get('http://petfriends.skillfactory.ru/my_pets')
-    div_all_my_pets = authorized_driver.find_element_by_id("all_my_pets")
-    assert div_all_my_pets is not None
-
-    title_founded = WebDriverWait(authorized_driver, 5).until(
-        EC.title_contains('PetFriends: My Pets')
-    )
-    assert title_founded
-
-    return authorized_driver
-
-
-# Присутствуют все питомцы.
-def test_check_my_pet(my_pets):
-    pet_tag = authorized_driver.find_element_by_xpath("//div[contains(@class,'task3')]/div[1]")
-    pet_count = int(pet_tag.text.split('\n')[1].split(':')[1])
-    pet_rows = authorized_driver.find_elements_by_xpath("//*[@id='all_my_pets']/table/tbody/tr")
-    assert pet_count == len(pet_rows)
-
-
-# Хотя бы у половины питомцев есть фото.
-def test_check_half_pets_have_photo(my_pets):
-    pet_images = my_pets.find_elements_by_xpath("//*[@id='all_my_pets']/table/tbody/tr/th/img")
-    counter = 0
-    for i in range(len(pet_images)):
-        if pet_images[i].get_attribute('src') != 'unknown':
-            counter += 1
-    assert counter >= len(pet_images) / 2
-
-
-# У всех питомцев есть имя, возраст и порода.
-def test_pet_has_name_kind_age(my_pets):
-    pet_data_complect = my_pets.find_elements_by_xpath("//*[@id='all_my_pets']/table/tbody/tr/td")
-
-    for pet_data in pet_data_complect:
-        assert pet_data.text.strip() != ''
-
-
-# У всех питомцев разные имена.
-def test_all_names_are_different(my_pets):
-    td_names = my_pets.find_elements_by_xpath("//*[@id='all_my_pets']/table/tbody/tr/td[1]")
-    pet_names = []
-    for td_name in td_names:
-        pet_names.append(td_name.text)
-    assert len(pet_names) == len(set(pet_names))
-
-
-# В списке нет повторяющихся питомцев. (Сложное задание).
-def test_all_pets_are_different(my_pets):
-    tr_pets = my_pets.find_elements_by_xpath("//*[@id='all_my_pets']/table/tbody/tr")
-    pets = []
-
-    for tr_pet in tr_pets:
-        pets.append(tr_pet.text)
-
-    assert len(set(pets)) == len(pets)
+   for i in range (len(pets)):
+      # Сравниваем количество питомцев на странице и количество питомцев из статистики
+      assert len(pets) == number
+      # Сравниваем количество питомцев с фото и без фото
+      assert len(with_photo) > len(without_photo)
+      # Сравниваем количество имён, пород и возрастов в массивах с количеством питомцев из статистики
+      assert len(names) == number
+      assert len(breed) == number
+      assert len(age) == number
+      # Проверяем уникальность имен питомцев
+      assert names[0].text != names[1].text
+      assert names[0].text != names[2].text
+      assert names[0].text != names[3].text
+      assert names[1].text != names[2].text
+      assert names[1].text != names[3].text
+      assert names[2].text != names[3].text
+      # Проверяем питомцев на уникальность
+      assert names[0].text != names[1].text and breed[0].text != breed[1].text and age[0].text != age[1].text
+      assert names[0].text != names[2].text and breed[0].text != breed[2].text and age[0].text != age[2].text
+      assert names[0].text != names[3].text and breed[0].text != breed[3].text and age[0].text != age[3].text
+      assert names[1].text != names[2].text and breed[1].text != breed[2].text and age[1].text != age[2].text
+      assert names[1].text != names[3].text and breed[1].text != breed[3].text and age[1].text != age[3].text
+      assert names[2].text != names[3].text and breed[2].text != breed[3].text and age[2].text != age[3].text
